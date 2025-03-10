ad29c36e60f4b96aab93d0f24a59903904e9c3b2
Sanchir01 <emgushovs@mail.ru>
2025-03-11 01:15:58 +0300

    add many update

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,43 @@
-use std::fmt::format;
-use actix_web::{get, App, HttpServer, Responder, web, HttpResponse};
+use std::env;
+
+use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
 use dotenvy::dotenv;
 use serde::{Deserialize, Serialize};
-use diesel::{pg::PgConnection,r2d2::{ Pool, self,ConnectionManager} ,prelude::*};
+use sqlx::PgPool;
 
-
-#[derive(Queryable,Serialize)]
+#[derive(Serialize, Deserialize)]
 struct Item {
-    id:i32,
-    name:String,
-    description:String,
+    id: i32,
+    name: String,
+    description: String,
 }
-
-#[derive(Serialize,Deserialize)]
-struct RequestItem{
-    name:String,
-    description:String,
+#[derive(Serialize, Deserialize)]
+struct RequestItem {
+    name: String,
+    description: String,
 }
-
 #[derive(Clone)]
-struct AppState{
-    db_pool:Pool<ConnectionManager<PgConnection>>
+struct AppState {
+    db_pool: PgPool,
 }
 
-impl AppState{}
+impl AppState {}
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     dotenv().ok().expect("Could not load .env file");
     println!("Starting server at http://localhost:8000");
 
-    HttpServer::new(|| {
-        App::new()
-            .service(greet)
-            .service(greet_name)
-    })
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL in not implented");
+    let db_pool = PgPool::connect(&database_url)
+        .await
+        .expect("Failded to connect to Postgres");
+    let app_state = AppState { db_pool };
+    HttpServer::new(|| App::new().service(greet).service(greet_name))
         .bind("0.0.0.0:8000")?
-        .workers(4).run().await
+        .workers(4)
+        .run()
+        .await
 }
 #[get("/hello")]
 async fn greet() -> impl Responder {
