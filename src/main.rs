df84ba6788ae23dffe0b4726c0df71add6997fe3
Sanchir01 <emgushovs@mail.ru>
2025-03-12 17:10:02 +0300

    need fix error

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
-use axum::extract::{Path, State};
 use dotenvy::dotenv;
 use serde::{Deserialize, Serialize};
-use sqlx::PgPool;
+use sqlx::postgres::PgPool;
 use std::env;
 use utoipa::ToSchema;
 mod servers;
@@ -33,8 +32,12 @@ async fn main() -> std::io::Result<()> {
     let db_pool = PgPool::connect(&database_url)
         .await
         .expect("Failed to connect to Postgres");
-    let app_state = AppState { db_pool };
-    run_http_server();
+
+
+    let poolsum = sqlx::query("SELECT 1 + 1 as sum").fetch_one(db_pool).await;
+    let sum : i32 = poolsum.get("sum");
+    println!("this sum {:?}", sum);
+    run_http_server().await;
     // let swagger = SwaggerUi::new("/swagger-ui/{_:.*}")
     //     .url("/api-docs/openapi.json", ApiDoc::openapi());
     Ok(())
